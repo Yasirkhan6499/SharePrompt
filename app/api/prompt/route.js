18ecf19b0acec442a7ad9194c4c3ed193edff3e6
Yasir Khan <Yasirkahn6499@gmail.com>
2023-08-09 05:40:49 +0100

    prompt routejs

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,18 +1,17 @@
 import Prompt from "@models/prompt";
-import { connectToDB } from "@utils/database"
+import { connectToDB } from "@utils/database";
 
+export const GET = async (request, response) => {
+  try {
+    await connectToDB();
 
-export const GET = async (request,response)=>{
-    try{
-        await connectToDB();
-
-         // Set appropriate cache-control headers
+    // Set appropriate cache-control headers
     response.setHeader("Cache-Control", "no-store, must-revalidate");
 
-        const prompts = await Prompt.find({}).populate("creator");
+    const prompts = await Prompt.find({}).populate("creator");
 
-        return new Response(JSON.stringify(prompts), {status: 200});
-    } catch (error){
-        return new Response("Failed to fetch all prompts", {status: 500});
-    }
-}
+    return new Response(JSON.stringify(prompts), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to fetch all prompts", { status: 500 });
+  }
+};
